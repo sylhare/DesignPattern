6098272c08f8a59de90bed1b71f010f84b003a9e
sylhare <sylhareng@gmail.com>
2017-09-29 08:45:11 -0400

    Adapter Pattern fix

---
+++ Pattern/src/pattern/structural/adapter/RetroPlayer.java
@@ -0,0 +1,10 @@
+package pattern.structural.adapter;
+
+/**
+ * Created by sylhare in 2017.
+ */
+public interface RetroPlayer {
+    void playRetro(String gameName);
+    void look8bits();
+    void link();
+}
